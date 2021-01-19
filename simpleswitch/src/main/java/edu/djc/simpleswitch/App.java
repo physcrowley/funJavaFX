75936beb6c37bbed97c12aac0aaec0ce4c5f8a9f
physcrowley <crowlda@ecolecatholique.ca>
2021-01-19 15:11:56 -0500

    description des graphes de scène

--- simpleswitch/src/main/java/edu/djc/simpleswitch/App.java
+++ simpleswitch/src/main/java/edu/djc/simpleswitch/App.java
@@ -26,6 +26,15 @@ public class App extends Application {
     	var btn1 = new Button("Change scene");
     	
     	layout1.getChildren().addAll(btn1, text1);
+
+    	/* graphe de scène 1
+    	 * 
+    	 * VBox layout1
+    	 * |
+    	 * |---Button btn1
+    	 * |
+    	 * `---Label text1 
+    	 */
     	
     	// 2nd scene graph
     	var layout2 = new HBox();
@@ -33,6 +42,15 @@ public class App extends Application {
     	var btn2 = new Button("Change scene");
     	
     	layout2.getChildren().addAll(text2, btn2);
+    	
+    	/* graphe de scène 2
+    	 * 
+    	 * HBox layout2
+    	 * |
+    	 * |---Label text2
+    	 * |
+    	 * `---Button btn2
+    	 */
     	
     	// initial scene
     	var displaying = new Scene(layout1, WIDTH, HEIGHT);
